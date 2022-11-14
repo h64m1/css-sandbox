a30dffe08793b9f2f44cb622151a83f8120c2a90
hmasui <29457120+h64m1@users.noreply.github.com>
2022-11-14 20:46:43 +0900

    StyledBoxにrefを追加、useElementSize hooksでcomponent sizeを取得

--- components/margin-padding/MarginPadding.tsx
+++ components/margin-padding/MarginPadding.tsx
@@ -1,3 +1,30 @@
+import { useElementSize } from '@mantine/hooks';
+import { StyledBox } from 'components/parts/StyledBox';
+import { PropsWithChildren } from 'react';
+
 export const MarginPadding = () => {
-  return <>margin padding</>;
+  // TODO: stateを作る
+  const height = 100;
+  const width = 600;
+
+  return (
+    <Margin height={height} width={width}>
+      margin padding
+    </Margin>
+  );
+};
+
+interface MarginProps {
+  height: number;
+  width: number;
+}
+
+const Margin = (props: PropsWithChildren<MarginProps>) => {
+  const { ref, width, height } = useElementSize();
+
+  return (
+    <StyledBox height={props.height} width={props.width} ref={ref}>
+      {props.children}
+    </StyledBox>
+  );
 };
--- components/parts/StyledBox.tsx
+++ components/parts/StyledBox.tsx
@@ -1,27 +1,37 @@
 import { Box } from '@mantine/core';
+import { forwardRef } from 'react';
+import { PropsWithChildren } from 'react';
 
 const defaultProps = {
   borderRadius: 2,
 };
 
 interface Props {
+  backgroundColor?: string;
+  border?: string;
   borderRadius?: number; // px
   height: number;
   width: number;
 }
 
-export const StyledBox = (props: Props) => {
-  return (
-    <Box
-      sx={(theme) => ({
-        // TODO: backgroundColorもpropsにする
-        backgroundColor: theme.colors.blue[2],
-        borderRadius: props.borderRadius ?? defaultProps.borderRadius,
-        height: props.height,
-        width: props.width,
-      })}
-    >
-      test
-    </Box>
-  );
-};
+export const StyledBox = forwardRef<HTMLDivElement, PropsWithChildren<Props>>(
+  (props: PropsWithChildren<Props>, ref) => {
+    return (
+      <Box
+        ref={ref}
+        sx={(theme) => ({
+          // TODO: backgroundColorもpropsにする
+          backgroundColor: props.backgroundColor ?? theme.colors.blue[2],
+          borderRadius: props.borderRadius ?? defaultProps.borderRadius,
+          border: props.border,
+          height: props.height,
+          width: props.width,
+        })}
+      >
+        {props.children}
+      </Box>
+    );
+  },
+);
+
+StyledBox.displayName = 'StyledBox';
