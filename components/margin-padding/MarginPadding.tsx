4550b1d5b03c50b99bd73008305074a9f41d6343
hmasui <29457120+h64m1@users.noreply.github.com>
2022-11-15 00:17:11 +0900

    margin, padding用コンポーネント仮おき

--- components/margin-padding/MarginPadding.tsx
+++ components/margin-padding/MarginPadding.tsx
@@ -1,29 +1,84 @@
-import { useElementSize } from '@mantine/hooks';
+import { useMantineTheme } from '@mantine/styles';
 import { StyledBox } from 'components/parts/StyledBox';
+import { forwardRef, useRef } from 'react';
 import { PropsWithChildren } from 'react';
 
 export const MarginPadding = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
   // TODO: stateを作る
   const height = 100;
   const width = 600;
+  const margin = 10;
+  const padding = 20;
+  const { colors } = useMantineTheme();
 
   return (
-    <Margin height={height} width={width}>
-      margin padding
+    <Margin
+      height={height}
+      width={width}
+      backgroundColor={colors.orange[4]}
+      ref={ref}
+    >
+      <Padding
+        height={height - margin}
+        width={width - margin}
+        backgroundColor={colors.green[4]}
+      >
+        <Base
+          height={height - margin - padding}
+          width={width - margin - padding}
+          backgroundColor={colors.gray[0]}
+        >
+          margin padding
+        </Base>
+      </Padding>
     </Margin>
   );
 };
 
-interface MarginProps {
+interface Props {
   height: number;
   width: number;
+  backgroundColor: string;
 }
 
-const Margin = (props: PropsWithChildren<MarginProps>) => {
-  const { ref, width, height } = useElementSize();
+const Margin = forwardRef<HTMLDivElement, PropsWithChildren<Props>>(
+  (props: PropsWithChildren<Props>, ref) => {
+    return (
+      <StyledBox
+        height={props.height}
+        width={props.width}
+        backgroundColor={props.backgroundColor}
+        ref={ref}
+      >
+        {props.children}
+      </StyledBox>
+    );
+  },
+);
 
+Margin.displayName = 'Margin';
+
+const Padding = (props: PropsWithChildren<Props>) => {
   return (
-    <StyledBox height={props.height} width={props.width} ref={ref}>
+    <StyledBox
+      height={props.height}
+      width={props.width}
+      backgroundColor={props.backgroundColor}
+    >
+      {props.children}
+    </StyledBox>
+  );
+};
+
+const Base = (props: PropsWithChildren<Props>) => {
+  return (
+    <StyledBox
+      height={props.height}
+      width={props.width}
+      backgroundColor={props.backgroundColor}
+    >
       {props.children}
     </StyledBox>
   );
