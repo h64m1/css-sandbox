f627a9db64ef3e0c31c9ee5d84aa401dcf5f43de
hmasui <29457120+h64m1@users.noreply.github.com>
2022-12-08 18:16:31 +0900

    margin表示用テキストを追加

--- components/aside/margin/MarginAside.tsx
+++ components/aside/margin/MarginAside.tsx
@@ -1,15 +1,27 @@
-import { Space } from '@mantine/core';
+import { Divider, Space } from '@mantine/core';
 import { MarginSelect } from 'components/aside/margin/select/MarginSelect';
 import { MarginSliders } from 'components/aside/margin/slider/MarginSliders';
+import { MarginText } from 'components/aside/margin/text/MarginText';
 
 export const MarginAside = () => {
   return (
     <>
-      <MarginSelect />
+      <MarginText />
 
-      <Space h="xl" />
+      <Divider
+        sx={{
+          marginTop: 24,
+          marginBottom: 12,
+        }}
+      />
 
-      <MarginSliders />
+      <>
+        <MarginSelect />
+
+        <Space h="xl" />
+
+        <MarginSliders />
+      </>
     </>
   );
 };
